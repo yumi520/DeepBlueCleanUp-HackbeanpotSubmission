43b7b315d133549b7cb80bfe9667a5b9b3834a33
Yumiko Chow <75456756+yumi520@users.noreply.github.com>
2024-02-25 01:38:38 -0500

    Transporting changes to this repo

--- README.md
+++ README.md
@@ -1,2 +1,43 @@
-# DeepBlueCleanUp
- 
+# Deep Blue Clean up
+
+An educational game where users can learn about what types of waste end up in the ocean.
+
+## Description
+
+Embark on an exciting and educational journey with "Deep Blue Clean up," 
+the ultimate game designed to teach players of all ages the importance of recycling 
+through interactive and engaging gameplay. 
+Dive into a virtual world where you'll discover the wonders of responsible 
+waste management and become a recycling hero!
+
+## Images
+<img width="546" alt="Screenshot of home page" src="assets/cleanup ss1.png">
+<img width="546" alt="Screenshot of second page" src="assets/cleanup ss2.png">
+<img width="546" alt="Screenshot of third page" src="assets/cleanup ss3.png">
+
+## Video
+
+
+## Authors
+
+Yumiko Chow - JS, HTML, CSS
+https://github.com/yumi520
+
+Jamie Li, HTML, CSS
+https://github.com/jli924
+
+## Version History
+
+* 0.1
+    * Initial Release
+
+
+## Acknowledgments
+
+Inspiration, code snippets, etc.
+* [awesome-readme](https://github.com/matiassingers/awesome-readme)
+* [JavaScript Drag and Drop](https://www.javascripttutorial.net/web-apis/javascript-drag-and-drop/)
+* [WHY SIX-PACK RINGS ARE MORE DANGEROUS THAN YOU THINK](https://www.seasandstraws.com/six-pack-rings.html)
+* [Plastic straws](https://www.plasticfreejuly.org/get-involved/what-you-can-do/plastic-straws/)
+* [How Scrap Metal Recycling is Giving New Hope to Ocean Life | Fraser Valley Metal Recycling](https://fvmr.ca/how-scrap-metal-recycling-is-giving-new-hope-to-ocean-life-fraser-valley-metal-recycling/)
+* [10 Ways to Reduce Plastic Pollution](https://www.nrdc.org/stories/10-ways-reduce-plastic-pollution)
---
+++ script.js
@@ -0,0 +1,143 @@
+/* draggable element */
+const item = document.querySelector('.item');
+const plasticbag = document.querySelector('.plasticbag');
+const cigarettes = document.querySelector('.cigarettes');
+const straw = document.querySelector('.straw');
+const sixpackring = document.querySelector('.sixpackring');
+const empty = document.querySelector('.empty');
+const metal = document.querySelector('.metal');
+const plasticwaterbottle = document.querySelector('.plasticwaterbottle');
+
+
+
+/*item.addEventListener('dragstart', dragStart);*/
+plasticbag.addEventListener('dragstart', dragStart);
+cigarettes.addEventListener('dragstart', dragStart);
+straw.addEventListener('dragstart', dragStart);
+sixpackring.addEventListener('dragstart', dragStart);
+empty.addEventListener('dragstart', dragStart);
+metal.addEventListener('dragstart', dragStart);
+plasticwaterbottle.addEventListener('dragstart', dragStart);
+
+function dragStart(e) {
+    e.dataTransfer.setData('text/plain', e.target.id);
+    setTimeout(() => {
+        e.target.classList.add('hide');
+    }, 0);
+}
+
+
+/* drop targets */
+const boxes = document.querySelectorAll('.box');
+
+boxes.forEach(box => {
+    box.addEventListener('dragenter', dragEnter)
+    box.addEventListener('dragover', dragOver);
+    box.addEventListener('dragleave', dragLeave);
+    box.addEventListener('drop', drop);
+});
+
+
+function dragEnter(e) {
+    e.preventDefault();
+    e.target.classList.add('drag-over');
+}
+
+function dragOver(e) {
+    e.preventDefault();
+    e.target.classList.add('drag-over');
+}
+
+function dragLeave(e) {
+    e.target.classList.remove('drag-over');
+}
+
+function drop(e) {
+    e.target.classList.remove('drag-over');
+
+    // get the draggable element
+    const id = e.dataTransfer.getData('text/plain');
+    const draggable = document.getElementById(id);
+
+    // add it to the drop target
+    e.target.appendChild(draggable);
+
+    // display the draggable element
+    draggable.classList.remove('hide');
+}
+
+const cigarettestextbox = document.querySelector('.cigarettes-text');
+const plasticbagtextbox = document.querySelector('.plasticbag-text');
+const strawtextbox = document.querySelector('.straw-text');
+const sixpackringtextbox = document.querySelector('.sixpackring-text');
+const metaltextbox = document.querySelector('.metal-text');
+const plasticwaterbottletextbox = document.querySelector('.plasticwaterbottle-text');
+
+
+document.querySelector('.cigarettes').addEventListener('dragstart', cigarettesfact);
+document.querySelector('.plasticbag').addEventListener('dragstart', plasticbagfact);
+document.querySelector('.straw').addEventListener('dragstart', strawfact);
+document.querySelector('.sixpackring').addEventListener('dragstart', sixpackringfact);
+document.querySelector('.metal').addEventListener('dragstart', metalfact);
+document.querySelector('.plasticwaterbottle').addEventListener('dragstart', plasticwaterbottlefact);
+
+//const allTextBoxes = [cigarettestextbox, plasticbagtextbox, strawtextbox,sixpackringtextbox, metaltextbox ,]
+
+/*function toggleFact(toggleFact){
+    allTextBoxes.forEach((textBox) => {
+        textBox.classList.add("hidden")
+    })
+    toggleFact.classList.toggle("hidden")
+}*/
+
+function cigarettesfact(){
+    cigarettestextbox.classList.toggle('hidden')
+    plasticbagtextbox.classList.add('hidden')
+    strawtextbox.classList.add('hidden')
+    sixpackringtextbox.classList.add('hidden')
+    metaltextbox.classList.add('hidden')
+    plasticwaterbottletextbox.classList.add('hidden')
+}
+
+function plasticbagfact(){
+    cigarettestextbox.classList.add('hidden')
+    plasticbagtextbox.classList.toggle('hidden')
+    strawtextbox.classList.add('hidden')
+    sixpackringtextbox.classList.add('hidden')
+    metaltextbox.classList.add('hidden')
+    plasticwaterbottletextbox.classList.add('hidden')
+}
+function strawfact(){
+    cigarettestextbox.classList.add('hidden')
+    plasticbagtextbox.classList.add('hidden')
+    strawtextbox.classList.toggle('hidden')
+    sixpackringtextbox.classList.add('hidden')
+    metaltextbox.classList.add('hidden')
+    plasticwaterbottletextbox.classList.add('hidden')
+}
+
+function sixpackringfact(){
+    cigarettestextbox.classList.add('hidden')
+    plasticbagtextbox.classList.add('hidden')
+    strawtextbox.classList.add('hidden')
+    sixpackringtextbox.classList.toggle('hidden')
+    metaltextbox.classList.add('hidden')
+    plasticwaterbottletextbox.classList.add('hidden')
+}
+function metalfact(){
+    cigarettestextbox.classList.add('hidden')
+    plasticbagtextbox.classList.add('hidden')
+    strawtextbox.classList.add('hidden')
+    sixpackringtextbox.classList.add('hidden')
+    metaltextbox.classList.toggle('hidden')
+    plasticwaterbottletextbox.classList.add('hidden')
+}
+
+function plasticwaterbottlefact(){
+    cigarettestextbox.classList.add('hidden')
+    plasticbagtextbox.classList.add('hidden')
+    strawtextbox.classList.add('hidden')
+    sixpackringtextbox.classList.add('hidden')
+    metaltextbox.classList.add('hidden')
+    plasticwaterbottletextbox.classList.toggle('hidden')
+}
